2d9c6e0a7b7ee772b4b642ff5006eb1ca37d06d6
Björn Svensson A <bjorn.d.svensson@gmail.com>
2018-09-26 10:30:51 +0200

    Adding flags and handling directories of videos

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tool to add creation time to video filenames
+
+## Prerequisite
+ffprobe : brew install ffmpeg
+
+
+## Install
+go install .
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 	"time"
@@ -10,35 +11,90 @@ import (
 	ffprobe "github.com/vansante/go-ffprobe"
 )
 
+// Find all files within given paths
+func getFiles(paths []string) ([]string, error) {
+	var files []string
+
+	for _, path := range paths {
+
+		fi, err := os.Stat(path)
+		if err != nil {
+			return files, err
+		}
+
+		switch mode := fi.Mode(); {
+		case mode.IsDir():
+			err = filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+					return err
+				}
+				if !info.IsDir() {
+					files = append(files, path)
+				}
+				return nil
+			})
+			if err != nil {
+				return files, err
+			}
+
+		case mode.IsRegular():
+			files = append(files, path)
+		}
+	}
+	return files, nil
+}
+
 func main() {
 
 	if len(os.Args) < 2 {
 		fmt.Println("Missing parameter, provide file name!")
 		return
 	}
-	file := os.Args[1]
 
-	if _, err := os.Stat(file); err != nil {
-		fmt.Printf("File error: %s\n", err)
+	force := flag.Bool("f", false, "Rename files")
+	showInfo := flag.Bool("i", false, "Show video metadata information")
+	flag.Parse()
+
+	files, err := getFiles(flag.Args())
+	if err != nil {
+		fmt.Printf("Error: %s", err)
 		return
 	}
 
-	data, err := ffprobe.GetProbeData(file, 500*time.Millisecond)
-	if err != nil {
-		log.Panicf("Error getting data: %v", err)
-	}
+	for _, file := range files {
 
-	t, err := time.Parse(time.RFC3339, data.Format.Tags.CreationTime)
-	if err != nil {
-		log.Panicf("Error converting date: %v", err)
-	}
+		data, err := ffprobe.GetProbeData(file, 500*time.Millisecond)
+		if err != nil {
+			fmt.Printf("Error getting data: %v", err)
+		}
 
-	newname := fmt.Sprintf("%d-%02d-%02d_%02d.%02d_%s",
-		t.Year(), t.Month(), t.Day(),
-		t.Hour(), t.Minute(), filepath.Base(file))
+		if *showInfo {
+			buf, _ := json.MarshalIndent(data, "", "  ")
+			fmt.Printf("%s: %s\n", file, string(buf))
+			break
+		}
 
-	err = os.Rename(file, newname)
-	if err != nil {
-		log.Panicf("Error renaming file: %s, err: %v", file, err)
+		t, err := time.Parse(time.RFC3339, data.Format.Tags.CreationTime)
+		if err != nil {
+			fmt.Printf("Error converting date: %v", err)
+			return
+		}
+
+		newname := fmt.Sprintf("%d-%02d-%02d_%02d.%02d_%s",
+			t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(),
+			filepath.Base(file))
+		newname = filepath.Join(filepath.Dir(file), newname)
+
+		if *force {
+			fmt.Printf("FORCE: Rename: %s to %s\n", file, newname)
+			err = os.Rename(file, newname)
+			if err != nil {
+				fmt.Printf("Error renaming file: %s, err: %v", file, err)
+				return
+			}
+		} else {
+			fmt.Printf("Rename: %s to %s\n", file, newname)
+		}
 	}
 }
